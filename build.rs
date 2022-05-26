c762fa828ba0b9b4cb0ef34212cf5175b10b5298
chrissly31415 <loschen@gmail.com>
2022-05-26 19:32:18 +0200

    updated relative directories

--- build.rs
+++ build.rs
@@ -5,7 +5,9 @@
 //https://github.com/rdkit/rdkit/blob/master/Code/MinimalLib/cffi_test.c
 
 extern crate bindgen;
-use std::path::PathBuf;
+use std::env;
+use std::path::{PathBuf, Path};
+use std::process::Command;
 
 //use std::env;
 
@@ -18,13 +20,44 @@ use std::path::PathBuf;
 //one need to set LD_LIBRARY_PATH manually if binary is called without cargo
 //e.g. export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/username/calc/rdkitcffi/lib/rdkitcffi_linux/linux-64
 
+fn download_rdkit_artefact() -> String {
+    //download rdkit cffi lib from azure
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    let cffi_url = "https://artprodsu6weu.artifacts.visualstudio.com/A85885aaa-9279-4937-b9af-77f592b58cf7/b9a21ad4-0deb-4a21-8386-996d0e642d94/_apis/artifact/cGlwZWxpbmVhcnRpZmFjdDovL3Jka2l0LWJ1aWxkcy9wcm9qZWN0SWQvYjlhMjFhZDQtMGRlYi00YTIxLTgzODYtOTk2ZDBlNjQyZDk0L2J1aWxkSWQvMTMwNy9hcnRpZmFjdE5hbWUvcmRraXRjZmZpX2xpbnV40/content?format=zip";
+    let cffi_zip= "rdkitcffi.zip";
+    let rdkit_branch = ".1.2021.09.11.0";
+    let shared_lib_name = "librdkitcffi.so";
+    let shared_lib_dir = format!("{}{}",out_dir,"/lib/rdkitcffi_linux/linux-64/");
+    let artefact_name = format!("{}{}", shared_lib_name, rdkit_branch);
+
+    Command::new("wget").args(&["-O", cffi_zip, cffi_url]).status().unwrap();
+    Command::new("unzip").args(&[ cffi_zip, "-d", &format!("{}{}",out_dir,"/lib")]).status().unwrap();
+    Command::new("ln").args(&[ "-s", &artefact_name, &format!("{}{}", shared_lib_dir, shared_lib_name)]).status().unwrap();
+    Command::new("ln").args(&[ "-s", &artefact_name, &format!("{}{}.1", shared_lib_dir, shared_lib_name)]).status().unwrap();
+
+    let shared_lib_path = Path::new(&shared_lib_name).join("shared_lib_dir");
+    
+    if !shared_lib_path.exists() {
+        println!("shared_lib_path: {:?}",shared_lib_path);
+    } else {
+        eprintln!("Could not download rdkti cffi shared library from:\n {}",cffi_url);
+        eprintln!("Consider downloading it manually!")
+    }
+
+    return shared_lib_dir.to_string();
+}
+
+
 fn main() {
-    let shared_lib_dir = "./lib/rdkitcffi_linux/linux-64/";
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    println!("out_dir: {:?}",out_dir);
+
+    let shared_lib_dir = download_rdkit_artefact();
     //let key = "LD_LIBRARY_PATH";
     //#env::set_var(key, shared_lib_dir);
 
     //this sets the dynamic lib path only during build
-    //println!("cargo:rustc-link-search={}", shared_lib_dir);
     println!("cargo:rustc-link-search=native={}", shared_lib_dir);
     println!("cargo:rustc-link-lib=dylib=rdkitcffi");
     
